8e68566e9194a521f49fc033528a1e5c57c9aba4
ilssn <ilssn@adswave.co>
2024-07-24 18:51:45 +0800

    fix: router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation'
 export default function HomePage() {
   const router = useRouter()
 	useEffect(() => {
-		router.replace('/ecom1' + window.location.search)
+		router.replace('/photoshow' + window.location.search)
 	}, [router]);
   return <div id="home-page">Welcome!</div>
 }
