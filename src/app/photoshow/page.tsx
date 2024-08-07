16fe45f2ab207040413f2157a82dc01cd366c65e
ilssn <ilssn@adswave.co>
2024-08-07 15:27:18 +0800

    upd: add chat server

--- src/app/photoshow/page.tsx
+++ src/app/photoshow/page.tsx
@@ -15,6 +15,25 @@ export default function PhotoshowPage() {
   const [tool, setTool] = React.useState<Tool>(tools[0])
   const [file, setFile] = React.useState<File | null>(null)
 
+
+  React.useEffect(() => {
+    const hostname = window.location.hostname;
+    const domain = hostname.split('.')[0];
+    if (!['demo'].includes(domain)) {
+      if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+        var script = document.createElement("script");
+        script.src = "https://assets.salesmartly.com/js/project_177_61_1649762323.js";
+        document.head.appendChild(script);
+      }
+    }
+
+    // 清除脚本
+    return () => {
+      const existingScript = document.querySelector("[src='https://assets.salesmartly.com/js/project_177_61_1649762323.js']");
+      existingScript && document.head.removeChild(existingScript);
+    };
+  }, []);
+
   return (
     <div id="photoshow-page" className="w-full p-4">
 
